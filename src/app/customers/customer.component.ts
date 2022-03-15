4ae172e1f7f58bfe7dfbc8503084810d5a4ba7d1
Aurélien BOUCHET <a.bouchet@groupeonepoint.com>
2022-03-15 10:51:15 +0100

    initilization and dev

---
+++ src/app/customers/customer.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
+  customer = new Customer();
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit()  {
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)
+      ]],
+      email: ['', [ Validators.required, Validators.email]],
+      sendCatalog: true
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+    firstName: 'Jack',
+    lastName: 'Simpson',
+    email: 'jack@toch.com',
+    sendCatalog: false
+    });
+  
+  }
+}
